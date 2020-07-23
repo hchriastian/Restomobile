88d988935b6ff6d199828484fe848fc65be4a5f7
hchriastian <55739504+hchriastian@users.noreply.github.com>
2020-07-23 11:47:48 +0700

    Add files via upload

---
+++ Infomedia.Mobile.Content.DataModel/ContentMobileContext.cs
@@ -0,0 +1,28 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+
+namespace Infomedia.Mobile.Content.DataModel
+{
+    public class ContentMobileContext<T> : DbContext where T : class
+    {
+        public ContentMobileContext()
+            : base("name=dbRestoMobileEntities")
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            throw new UnintentionalCodeFirstException();
+        }
+
+        private DbSet<T> _dbEntities;
+
+        /// <summary>
+        /// Entities contained by this repository.
+        /// </summary>        
+        public DbSet<T> DBEntities
+        {
+            get { return _dbEntities ?? (_dbEntities = this.Set<T>()); }
+        }
+    }
+}
